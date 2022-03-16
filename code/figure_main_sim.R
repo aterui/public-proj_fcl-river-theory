9b5cc3cc57cb9474c72c3666afadab1408b1b923
aterui <hanabi0111@gmail.com>
2022-03-16 11:43:29 -0400

    edit: figures

--- code/figure_main_sim.R
+++ code/figure_main_sim.R
@@ -1,8 +1,10 @@
+
 # setup -------------------------------------------------------------------
 
 rm(list = ls(all.names = TRUE))
 source(here::here("code/library.R"))
 source(here::here("code/figure_theme.R"))
+
 
 # analysis ----------------------------------------------------------------
 
@@ -18,7 +20,23 @@ df_sim <- sim_main_result %>%
                          e_bp == 4 ~ "Strong"),
          igp = factor(igp, levels = c("Chain",
                                       "Weak",
-                                      "Strong")))
+                                      "Strong")),
+         productivity = recode(r_b,
+                               `4` = sprintf('"Low productivity"~(r[b]=="%.2f")',
+                                             r_b),
+                               `8` = sprintf('"High productivity"~(r[b]=="%.2f")',
+                                             r_b)),
+         productivity = factor(productivity,
+                               levels = rev(levels(factor(productivity)))),
+         disturb = recode(p_disturb,
+                          `0.01` = sprintf('"Low disturbance"~(p[m]=="%.2f")',
+                                           p_disturb),
+                          `0.1` = sprintf('"High disturbance"~(p[m]=="%.2f")',
+                                          p_disturb)),
+         disturb = factor(disturb,
+                          levels = rev(levels(factor(disturb))))
+         )
+
 
 # figure ------------------------------------------------------------------
 
@@ -32,8 +50,9 @@ g_np <- df_sim %>%
               size = 0.5,
               color = "salmon",
               fill = "salmon") +
-  facet_grid(rows = vars(p_disturb),
-             cols = vars(r_b)) +
+  facet_grid(rows = vars(disturb),
+             cols = vars(productivity),
+             labeller = label_parsed) +
   labs(x = "Ecosystem size (number of patches)",
        y = "Food chain length",
        linetype = "IGP")
@@ -51,8 +70,9 @@ g_pb <- df_sim %>%
               size = 0.5,
               color = "steelblue",
               fill = "steelblue") +
-  facet_grid(rows = vars(p_disturb),
-             cols = vars(r_b)) +
+  facet_grid(rows = vars(disturb),
+             cols = vars(productivity),
+             labeller = label_parsed) +
   labs(x = "Ecosystem complexity (branching prob.)",
        y = "Food chain length",
        linetype = "IGP")
---
+++ code/figure_si_fcl.R
@@ -0,0 +1,95 @@
+
+# setup -------------------------------------------------------------------
+
+rm(list = ls(all.names = TRUE))
+source(here::here("code/library.R"))
+source(here::here("code/figure_theme.R"))
+
+
+# analysis ----------------------------------------------------------------
+
+load(file = here::here("output/result_main.RData"))
+
+df_sim <- sim_main_result %>% 
+  group_by(param_set) %>% 
+  filter(!(theta == 1 &
+           sd_disturb_source == 5 &
+           sd_disturb_lon == 0.1)) %>% 
+  mutate(igp = case_when(e_bp == 0 ~ "Chain",
+                         e_bp == 2 ~ "Weak",
+                         e_bp == 4 ~ "Strong"),
+         igp = factor(igp, levels = c("Chain",
+                                      "Weak",
+                                      "Strong")),
+         productivity = recode(r_b,
+                               `4` = sprintf('"Low productivity"~(r[b]=="%.2f")',
+                                             r_b),
+                               `8` = sprintf('"High productivity"~(r[b]=="%.2f")',
+                                             r_b)),
+         productivity = factor(productivity,
+                               levels = rev(levels(factor(productivity)))),
+         disturb = recode(p_disturb,
+                          `0.01` = sprintf('"Low disturbance"~(p[m]=="%.2f")',
+                                           p_disturb),
+                          `0.1` = sprintf('"High disturbance"~(p[m]=="%.2f")',
+                                          p_disturb)),
+         disturb = factor(disturb,
+                          levels = rev(levels(factor(disturb))))
+  )
+
+df_param <- expand.grid(theta = 0.1,
+                        sd_disturb_source = c(0.1, 5),
+                        sd_disturb_lon = c(0.1, 5)) %>% 
+  as_tibble() %>% 
+  filter(sd_disturb_source != sd_disturb_lon)
+
+
+# figure ------------------------------------------------------------------
+
+theme_set(plt_theme)
+
+list_g_np <- foreach(i = seq_len(nrow(df_param))) %do% {
+  
+  g_np <- df_sim %>% 
+    filter(theta == df_param$theta[i],
+           sd_disturb_lon == df_param$sd_disturb_lon[i],
+           sd_disturb_source == df_param$sd_disturb_source[i]) %>% 
+    ggplot(aes(x = n_patch,
+               y = fcl,
+               linetype = igp)) +
+    geom_smooth(method = "loess",
+                size = 0.5,
+                color = "salmon",
+                fill = "salmon") +
+    facet_grid(rows = vars(disturb),
+               cols = vars(productivity),
+               labeller = label_parsed) +
+    labs(x = "Ecosystem size (number of patches)",
+         y = "Food chain length",
+         linetype = "IGP")
+  
+  return(g_np)
+}
+
+list_g_pb <- foreach(i = seq_len(nrow(df_param))) %do% {
+
+  g_pb <- df_sim %>% 
+    filter(theta == df_param$theta[i],
+           sd_disturb_lon == df_param$sd_disturb_lon[i],
+           sd_disturb_source == df_param$sd_disturb_source[i]) %>% 
+    ggplot(aes(x = p_branch,
+               y = fcl,
+               linetype = igp)) +
+    geom_smooth(method = "loess",
+                size = 0.5,
+                color = "steelblue",
+                fill = "steelblue") +
+    facet_grid(rows = vars(disturb),
+               cols = vars(productivity),
+               labeller = label_parsed) +
+    labs(x = "Ecosystem complexity (branching prob.)",
+         y = "Food chain length",
+         linetype = "IGP")
+  
+  return(g_pb)
+}
