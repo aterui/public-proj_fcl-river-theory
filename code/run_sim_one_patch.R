a429161b0cd674f3d90d60f03ce7c223fccf2266
aterui <hanabi0111@gmail.com>
2023-02-06 12:43:18 -0500

    edit: run one patch sim

--- code/run_sim_one_patch.R
+++ code/run_sim_one_patch.R
@@ -13,11 +13,12 @@ registerDoSNOW(cl)
 df_param <- expand.grid(n_timestep = 1000,
                         n_warmup = 200,
                         n_burnin = 400,
-                        r_b = c(2, 20),
+                        r_b = seq(2, 20, length = 4),
                         k = c(100, 500),
                         e = 1, # to conv_eff[1]
-                        a = seq(0.001, 0.5, length = 30),
-                        h = seq(0, 2, length = 30),
+                        a1 = seq(0, 0.5, length = 20),
+                        a2 = seq(0, 0.25, length = 20),
+                        h = seq(0, 1, by = 0.25),
                         s = c(0, 0.5)) %>%
   mutate(param_set = seq_len(nrow(.))) %>% 
   as_tibble()
@@ -25,7 +26,8 @@ df_param <- expand.grid(n_timestep = 1000,
 # run simulation ----------------------------------------------------------
 tic()
 df_fcl <- foreach(i = 1:nrow(df_param),
-                  .combine = bind_rows) %dopar%{
+                  .combine = bind_rows,
+                  .packages = "dplyr") %dopar%{
                     
                     x <- df_param[i,]
                     dyn <- suppressMessages(mcbrnet::igpsim(n_patch = 10,
@@ -34,15 +36,31 @@ df_fcl <- foreach(i = 1:nrow(df_param),
                                                             n_timestep = x$n_timestep,
                                                             r_b = x$r_b,
                                                             conv_eff = x$e,
-                                                            attack_rate = x$a,
+                                                            attack_rate = c(x$a1,
+                                                                            x$a2,
+                                                                            x$a1),
                                                             handling_time = x$h,
                                                             s = x$s,
                                                             carrying_capacity = x$k,
                                                             p_disturb = 0,
-                                                            m_disturb = 0,
+                                                            i_disturb = 0,
                                                             p_dispersal = 0))
                     
-                    re <- dplyr::tibble(x, fcl = dyn$df_patch$fcl)
+                    state <- dyn$df_dynamics %>% 
+                      group_by(species, patch_id) %>% 
+                      slice(which.max(timestep)) %>% 
+                      ungroup() %>% 
+                      mutate(y0 = as.numeric(abundance > 0),
+                             lambda = case_when(species != "ig-predator" ~ 1,
+                                                species == "ig-predator" ~ 2),
+                             y = y0 * lambda) %>%
+                      group_by(patch_id) %>% 
+                      summarize(state = sum(y)) %>% 
+                      pull(state)
+                      
+                    re <- dplyr::tibble(x,
+                                        fcl = dyn$df_patch$fcl,
+                                        state = state)
                     
                     return(re)                 
                   }
