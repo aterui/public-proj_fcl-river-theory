868e50b29173045538379319f295825497f0e6b7
aterui <hanabi0111@gmail.com>
2023-02-06 12:43:31 -0500

    edit: figure one patch

--- code/figure_si_one_patch.R
+++ code/figure_si_one_patch.R
@@ -1,54 +1,64 @@
 
 # setup -------------------------------------------------------------------
 
-rm(list = ls(all.names = TRUE))
+rm(list = ls())
 source(here::here("code/library.R"))
 source(here::here("code/figure_theme.R"))
 
 
 # data --------------------------------------------------------------------
 
-lab1 <- c(`0` = "No~switching~(s==0)",
-          `0.5` = "Strong~switching~(s==0.5)")
+lab <- list(c(`0` = "No~switching~(s==0)",
+              `0.5` = "Weak~switching~(s==0.5)",
+              `1` = "Strong~switching~(s==1)"),
+            c(`2` = "r[B]==2",
+              `8` = "r[B]==8",
+              `14` = "r[B]==14",
+              `20` = "r[B]==20"),
+            c(`100` = "K==100",
+              `500` = "K==500"))
 
-lab2 <- c(`2` = "r[B]==2",
-          `20` = "r[B]==20")
+df_fcl <- readRDS(here::here("output/sim_one_patch.rds"))
+h_set <- unique(df_fcl$h)
 
-lab3 <- c(`100` = "K==100",
-          `500` = "K==500")
+foreach(i = 1:length(h_set)) %do% {
+  
+  g_one <- df_fcl %>% 
+    group_by(param_set) %>% 
+    summarize(across(.fns = unique, .cols = -c(fcl, state)),
+              state = max(state)) %>% 
+    filter(h == h_set[i]) %>% 
+    ggplot(aes(x = a1,
+               y = a2,
+               fill = factor(state))) +
+    geom_raster(alpha = 0.8) +
+    geom_point(data = expand.grid(a1 = 0.1,
+                                  a2 = c(0.01, 0.05),
+                                  state = 0)) +
+    facet_grid(rows = vars(r_b),
+               cols = vars(s, k), labeller = labeller(s = as_labeller(lab[[1]], label_parsed),
+                                                      r_b = as_labeller(lab[[2]], label_parsed),
+                                                      k = as_labeller(lab[[3]], label_parsed))) +
+    #scale_fill_met_d("Hiroshige",  direction = -1) +
+    scale_fill_viridis_d(limits = factor(0:4)) +
+    labs(x = expression("Attack rate ("*a[CP]*")"),
+         y = expression("Attack rate ("*a[BP]*")"),
+         fill = "State") +
+    theme_classic() +
+    theme(strip.background = element_blank(),
+          strip.text = element_text(size = 12), 
+          axis.title = element_text(size = 14),
+          axis.text = element_text(size = 10))
+  
+  # export ------------------------------------------------------------------
+  
+  ggsave(g_one, 
+         filename = here::here(paste0("figure/figure_one_patch_h",
+                                      h_set[i] * 100,
+                                      ".pdf")),
+         width = 12,
+         height = 11)
+  
+}
 
-g_one <- readRDS(here::here("output/sim_one_patch.rds")) %>% 
-  group_by(param_set) %>% 
-  summarize(across(.fns = unique, .cols = -fcl),
-            fcl = median(fcl)) %>% 
-  ggplot(aes(x = a,
-             y = h,
-             fill = fcl)) +
-  geom_raster(alpha = 0.8) +
-  geom_point(data = expand.grid(a = c(0.025, 0.25),
-                                h = c(0.5, 1.25),
-                                fcl = 0) %>% 
-               filter(!(a != 0.025 & h == 0.5)),
-             color = "black") +
-  facet_grid(rows = vars(r_b),
-             cols = vars(s, k), labeller = labeller(s = as_labeller(lab1, label_parsed),
-                                                    r_b = as_labeller(lab2, label_parsed),
-                                                    k = as_labeller(lab3, label_parsed))) +
-  MetBrewer::scale_fill_met_c("Hiroshige",
-                              direction = -1) +
-  labs(x = "Attack rate (a)",
-       y = "Handling time (h)",
-       fill = "FCL") +
-  theme_classic() +
-  theme(strip.background = element_blank(),
-        strip.text = element_text(size = 12), 
-        axis.title =  element_text(size = 14),
-        axis.text =  element_text(size = 10))
 
-
-# export ------------------------------------------------------------------
-
-ggsave(g_one, 
-       filename = here::here("figure/figure_one_patch.pdf"),
-       width = 12,
-       height = 6)
