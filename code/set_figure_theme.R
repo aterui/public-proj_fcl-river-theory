5d80408a2dd8b953850aa086c14653467a9d132e
aterui <hanabi0111@gmail.com>
2023-02-07 12:51:40 -0500

    rename

--- code/set_figure_theme.R
+++ code/set_figure_theme.R
@@ -3,7 +3,7 @@ library(tidyverse)
 plt_theme <- theme_bw() + theme(
   plot.background = element_blank(),
   
-  panel.background = element_rect(grey(0.98)),
+  panel.background = element_rect(grey(0.96)),
   panel.border = element_rect(fill = NA),
   
   panel.grid = element_blank(),
@@ -15,8 +15,9 @@ plt_theme <- theme_bw() + theme(
   panel.grid.minor.x = element_blank(),
   panel.grid.minor.y = element_blank(),
   
+  axis.text = element_text(size = 10),
+    
   strip.background = element_blank(),
-  strip.text.x = element_text(size = 10),
-  strip.text.y = element_text(size = 10),
-  axis.title = element_text(size = 10)
+  strip.text = element_text(size = 12),
+  axis.title = element_text(size = 14)
 )
