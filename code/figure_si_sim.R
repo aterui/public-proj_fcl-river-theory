585afe3f705f34db2fc9a6cab0ddefb00d16658c
AkiraTerui <hanabi0111@gmail.com>
2023-02-09 00:09:56 -0500

    edit: figures

--- code/figure_si_sim.R
+++ code/figure_si_sim.R
@@ -11,6 +11,7 @@ lapply(list("code/library.R",
 ## filter s & mean_disturb_source for visualization
 s_set <- 0.5
 mu_disturb <- 0.4
+r_set <- c(8, 20)
 
 ## df for heatmap
 df_heat <- df_param %>% 
@@ -22,11 +23,13 @@ df_heat <- df_param %>%
 ## df for gam plot
 df_plot <- df_sim %>% 
   filter(mean_disturb_source == mu_disturb,
-         (s == 0 & a_bp == 0) | s == s_set)
+         (s == 0 & a_bp == 0) | s == s_set,
+         r_b %in% r_set)
 
 
 # heatmap -----------------------------------------------------------------
 
+theme_set(plt_theme)
 r_set <- c(8, 20)
 df_point <- expand.grid(r_b = r_set,
                         p_disturb = unique(df_sim$p_disturb),
@@ -73,63 +76,45 @@ g_m <- (g_size + g_branch) + plot_annotation(tag_levels = "A")
 
 # gam plot ----------------------------------------------------------------
 
-## ecosystem size effect
-g_gam_np <- foreach(i = 1:length(r_set)) %do% {
-  
-  df_plot %>% 
-    filter(r_b == r_set[i]) %>% 
-    ggplot(aes(x = n_patch,
-               y = fcl,
-               color = factor(p_disturb),
-               fill = factor(p_disturb))) +
-    geom_smooth(method = "gam") +
-    facet_grid(rows = vars(omn),
-               cols = vars(disp),
-               scales = "free") +
-    labs(y = "Food chain length",
-         x = "Ecosystem size (number of habitat patches)",
-         color = "Disturbance prob.",
-         fill = "Disturbance prob.") +
-    scale_color_met_d("Hiroshige", direction = -1) +
-    scale_fill_met_d("Hiroshige", direction = -1) +
-    ggtitle(ifelse(r_set[i] == min(r_set),
-                   expression("Low productivity ("*r[b]~"= 8)"),
-                   expression("High productivity ("*r[b]~"= 20)")))
-}
+lab <- c(`8` = "Low~productivity~(r[b]==8)",
+         `20` = "High~productivity~(r[b]==20)")
 
-g_np <- (g_gam_np[[1]] + g_gam_np[[2]]) + 
-  plot_annotation(tag_levels = "A") +
-  plot_layout(guides = "collect")
+## ecosystem size effect
+g_np <-  df_plot %>% 
+  ggplot(aes(x = n_patch,
+             y = fcl,
+             color = factor(p_disturb),
+             fill = factor(p_disturb))) +
+  geom_smooth(method = "gam") +
+  facet_grid(rows = vars(omn),
+             cols = vars(disp, r_b),
+             scales = "free",
+             labeller = labeller(r_b = as_labeller(lab, label_parsed))) +
+  labs(y = "Food chain length",
+       x = "Ecosystem size (number of habitat patches)",
+       color = "Disturbance prob.",
+       fill = "Disturbance prob.") +
+  scale_color_met_d("Hiroshige", direction = -1) +
+  scale_fill_met_d("Hiroshige", direction = -1)
 
 ## ecosystem complexity effect
-g_gam_pb <- foreach(i = 1:length(r_set)) %do% {
-  
-  df_plot %>% 
-    filter(r_b == r_set[i]) %>% 
-    ggplot(aes(x = p_branch,
-               y = fcl,
-               color = factor(p_disturb),
-               fill = factor(p_disturb))) +
-    geom_smooth(method = "gam") +
-    facet_grid(rows = vars(omn),
-               cols = vars(disp),
-               scales = "free") +
-    labs(y = "Food chain length",
-         x = "Ecosystem complexity (branching probability)",
-         color = "Disturbance prob.",
-         fill = "Disturbance prob.") +
-    scale_x_continuous(breaks = c(0.1, 0.4, 0.7, 1)) +
-    scale_color_met_d("Hiroshige", direction = -1) +
-    scale_fill_met_d("Hiroshige", direction = -1) +
-    ggtitle(ifelse(r_set[i] == min(r_set),
-                   expression("Low productivity ("*r[b]~"= 8)"),
-                   expression("High productivity ("*r[b]~"= 20)")))
-}
-
-g_pb <- (g_gam_pb[[1]] + g_gam_pb[[2]]) + 
-  plot_annotation(tag_levels = "A") +
-  plot_layout(guides = "collect")
-
+g_pb <-  df_plot %>% 
+  ggplot(aes(x = p_branch,
+             y = fcl,
+             color = factor(p_disturb),
+             fill = factor(p_disturb))) +
+  geom_smooth(method = "gam") +
+  facet_grid(rows = vars(omn),
+             cols = vars(disp, r_b),
+             scales = "free",
+             labeller = labeller(r_b = as_labeller(lab, label_parsed))) +
+  labs(y = "Food chain length",
+       x = "Ecosystem complexity (branching probability)",
+       color = "Disturbance prob.",
+       fill = "Disturbance prob.") +
+  scale_x_continuous(breaks = c(0.1, 0.4, 0.7, 1)) +
+  scale_color_met_d("Hiroshige", direction = -1) +
+  scale_fill_met_d("Hiroshige", direction = -1)
 
 # export ------------------------------------------------------------------
 
